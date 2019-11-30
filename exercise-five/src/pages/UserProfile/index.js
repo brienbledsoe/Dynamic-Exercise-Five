145805da9a5663c92f07fb7a93412d1d65a59f1f
Brien Bledsoe <bab619@nyu.edu>
2019-11-29 22:16:28 -0500

    I have no idea what is going on

--- exercise-five/src/pages/UserProfile/index.js
+++ exercise-five/src/pages/UserProfile/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 //this is the componenet that we're pushing here
 import { UserInformation } from "../../components/UserProfileComponents"
 
-export default function UserProfile(){
+export default function UserProfile() {
   return(
     <div>
       <div>UserProfile</div>
