9c42d1f5ef1f11903891539c875793c81b95ca55
Brien Bledsoe <bab619@nyu.edu>
2019-11-29 19:03:21 -0500

    correcting import path for UserInformation in UserProfile index.js file

--- exercise-five/src/pages/UserProfile/index.js
+++ exercise-five/src/pages/UserProfile/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 //this is the componenet that we're pushing here
-import {UserInformation} from "../..components/UserProfileComponents"
+import { UserInformation } from "../../components/UserProfileComponents"
 
 export default function UserProfile(){
   return(
     <div>
       <div>UserProfile</div>
-      <UserInformation /> 
+      <UserInformation />
     </div>
   )
 
