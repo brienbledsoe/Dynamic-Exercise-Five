3a72f08c2101e01b084da8d535cebc7c3fdc914e
Brien Bledsoe <bab619@nyu.edu>
2019-11-29 19:07:30 -0500

    adding closing tag to header in the header components index.js file

--- exercise-five/src/components/Header/index.js
+++ exercise-five/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header(){
   return(
-    <header className="Header>"
+    <header className="Header">
     <nav>
       {loggedIn && <a href="/">Home</a>}
       {!loggedIn && <a href="/login">Login</a>}
