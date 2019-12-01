d42ec999d7ebb8b12d2abb14803fd56bcdb855d6
Brien Bledsoe <bab619@nyu.edu>
2019-12-01 16:05:23 -0500

    adding the index.js files to the end of the import lines in app.js file

--- exercise-five/src/App.js
+++ exercise-five/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+
+//Components & Pages to IMPORT
 import {Route,BrowserRouter as Router} from 'react-router-dom'
-import Header from "./components/Header"
-import Login from "./pages/Login"
-import Logout from "./pages/Logout"
-import Signup from "./pages/Signup"
-import UserProfile from "./pages/UserProfile"
+import Header from "./components/Header";
+import Login from "./pages/Login/index.js";
+import Logout from "./pages/Logout/index.js";
+import Signup from "./pages/Signup/index.js";
+import UserProfile from "./pages/UserProfile/index.js";
 
 
 function App() {
@@ -13,11 +15,10 @@ function App() {
     <div className="App">
     <Header loggedIn={false}/>
       <Router>
-          <Route exact path="/" component={UserProfile} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/sign-up" component={Signup} />
-          <Route exact path="/log-out" component={Logout} />
-
+          <Route exact path="/" component= {UserProfile} />
+          <Route exact path="/login" component= {Login} />
+          <Route exact path="/sign-up" component= {Signup} />
+          <Route exact path="/log-out" component= {Logout} />
       </Router>
     </div>
   );
--- exercise-five/src/pages/UserProfile/index.js
+++ exercise-five/src/pages/UserProfile/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 //this is the componenet that we're pushing here
-import { UserInformation } from "../../components/UserProfileComponents"
 
 export default function UserProfile() {
   return(
     <div>
-      <div>UserProfile</div>
-      <UserInformation />
+      <div id="user">UserProfile</div>
+
     </div>
   );
 
