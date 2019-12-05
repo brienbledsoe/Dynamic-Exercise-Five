edde748a6cea61e5ce416ee5f60bbc293f2cae5f
Brien Bledsoe <bab619@nyu.edu>
2019-12-05 15:47:59 -0500

    fixed copomenet being exported in userprofilecomponeents

--- exercise-five/src/pages/UserProfile/index.js
+++ exercise-five/src/pages/UserProfile/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 //this is the componenet that we're pushing here
-import { UserInformation } from "../../components/UserProfileComponents"
+import UserProfileInfo  from "../../components/UserProfileComponents"
 
 export default function UserProfile({ user }) {
   console.log("user",user);
   return(
     <div>
       <h1>UserProfile for {user.uid && user.uid}</h1>
-      <userInformation email={user.email ? user.email : "whoops"} />
+      <UserProfileInfo email={user.email ? user.email : "whoops"} />
     </div>
   );
 
